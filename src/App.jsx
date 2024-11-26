bf77d25d135d3d3687971c25cd4062dcdbcf39c9
l1nkh <diogo.m.henriques98@gmail.com>
2024-11-26 00:13:12 +0000

    Start developing hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Hero from './components/Hero.jsx'
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-blue-500 font-bold">Welcome to this website</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
     </main>
   )
 }
