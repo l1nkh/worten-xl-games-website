726749c1429cbd45970ff5c4f8076a95848687a2
l1nkh <diogo.m.henriques98@gmail.com>
2024-11-28 01:14:13 +0000

    Add hero section scroll animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -6,7 +14,7 @@ const Hero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 3;
+    const totalVideos = 4;
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () => {
@@ -19,10 +27,71 @@ const Hero = () => {
         setCurrentIndex(upcomingVideoIndex);
     }
 
+    useEffect(() => {
+        if(loadedVideos === totalVideos - 1){
+            setIsLoading(false);
+        }
+    }, [loadedVideos])
+
+    useGSAP(() => {
+        if(hasClicked) {
+            gsap.set('#next-video', {visibility: 'visible'})
+
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale:1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play(),
+            })
+
+            gsap.from('#current-video', {
+                transformOrigin: 'center center',
+                scale:0,
+                duration: 1.5,
+                ease: 'power1.inOut',
+            })
+        }
+    }, {dependencies: [currentIndex], revertOnUpdate: true});
+
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(25% 14%, 2% 13%, 30% 52%, 0 94%, 30% 91%, 50% 74%, 69% 92%, 99% 96%, 68% 54%, 95% 11%, 73% 14%, 49% 38%)',
+            borderRadius: '0 0 10% 15%',
+            transition: '1.5s ease',
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(20% 0%, 0 0, 0 53%, 0 100%, 20% 100%, 50% 100%, 80% 100%, 100% 100%, 100% 48%, 100% 0, 80% 0%, 52% 0)',
+            borderRadius: '0 0 0 0',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+
+    }, {dependencies: [currentIndex], revertOnUpdate: true});
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+            {isLoading && (
+                <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden'>
+                    <div className='three-body'>
+                        <div className='three-body__dot'/>
+                        <div className='three-body__dot'/>
+                        <div className='three-body__dot'/>
+                    </div>
+                </div>
+            )}
+
             <div id='video-frame' className='relative z-10 h-dvh w-screen
             overflow-hidden rounded-lg bg-blue-75'>
                 <div>
@@ -71,7 +140,7 @@ const Hero = () => {
                 z-40 text-blue-75'>
                     G<b>a</b>ming
                 </h1>
-                
+
                 <div className='absolute left-0 top-0 z-40 size-full'>
                     <div className='mt-24 px-5 sm:px-10'>
                         <h1 className='special-font hero-heading text-blue-100'>
@@ -80,9 +149,23 @@ const Hero = () => {
                         <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
                             Enter the Metagame Layer <br /> Unleash the Play Economy
                         </p>
+
+                        <Button
+                            id="watch-trailer"
+                            title="Watch Trailer"
+                            leftIcon={<TiLocationArrow />}
+                            containerClass="!bg-yellow-300"
+                            flex-center
+                            gap-1
+                        />
                     </div>
                 </div>
             </div>
+
+            <h1 className='special-font hero-heading absolute bottom-5 right-5
+            text-black-75'>
+                G<b>a</b>ming
+            </h1>
         </div>
     )
 }
