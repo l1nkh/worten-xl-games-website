2deebae8c33dfc0a4d487b502b4d37a24b8d8d2f
l1nkh <diogo.m.henriques98@gmail.com>
2024-11-29 01:02:53 +0000

    Add AnimatedTitle component
    Animated title to be reused throughout the site

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnimatedTitle = ({title, containerClass}) => {
+  return (
+    <div className='mt-5 text-center text-4xl uppercase leading-[0.8]
+    md:text-[6rem]'>
+        {title}
+    </div>
+  )
+}
+
+export default AnimatedTitle
