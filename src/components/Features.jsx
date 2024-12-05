fc52ef5f95f73e3d1114f3feaa1584a671ba5d12
l1nkh <diogo.m.henriques98@gmail.com>
2024-12-05 17:38:55 +0000

    Finish bentoBox section of the website

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { TiLocationArrow } from 'react-icons/ti';
 
-const BentoCard = ({src, title, description, isComingSoon}) => {
-    return(
+const BentoCard = ({ src, title, description }) => {
+    return (
         <div className='relative size-full'>
-            <video 
+            <video
                 src={src}
                 loop
                 muted
                 autoPlay
                 className='absolute left-0 top-0 size-full object-cover object'
             />
-            {title}
+            <div className="relative z-10 flex size-full flex-col justify-between
+            p-5 text-blue-50">
+                <div>
+                    <h1 className='bento-title special-font'>
+                        {title}
+                    </h1>
+                    {
+                        description &&
+                        (<p className='mt-3 max-w-64 text-xs md:text-base'>
+                            {description}
+                        </p>)
+                    }
+                </div>
+            </div>
         </div>
     )
 }
@@ -38,9 +52,59 @@ const Features = () => {
                         description="A cross-platform metagame app, turning app,
                         turning your activities across Web2 and Web3 games into 
                         a rewarding adventure."
-                        isComingSoon
                     />
                 </div>
+
+                <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+                    <div className="bento-tilt_1 row-span-1 md:col-span-1
+                    md:row-span-2">
+                        <BentoCard
+                            src="videos/feature-2.mp4"
+                            title={<>zig<b>m</b>a</>}
+                            description="An anime and gaming-inspired NFT collection
+                             - the IP primed for"
+                        />
+                    </div>
+
+                    <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1
+                    md:ms-0">
+                        <BentoCard
+                            src="videos/feature-3.mp4"
+                            title={<>n<b>e</b>xus</>}
+                            description="A gamified social hub, adding a new dimension
+                            of play to social interaction for Web3 communities."
+                        />
+                    </div>
+                    <div className="bento-tilt_1 me-32 md:col-span-1
+                    md:me-0">
+                        <BentoCard
+                            src="videos/feature-4.mp4"
+                            title={<>az<b>u</b>l</>}
+                            description="A cross-world AI Agent - elevating your
+                            gameplay to be more fun and productive."
+                        />
+                    </div>
+                    <div className="bento-tilt_2">
+                        <div className="flex size-full flex-col justify-between
+                        bg-violet-300 p-5">
+                            <h1 className='bento-title special-font max-w-64 text-black'>
+                                M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
+                            </h1>
+                            <TiLocationArrow className='m-5 scale-[5] self-end' />
+                        </div>
+                    </div>
+
+                    <div className="bento-tilt_2">
+                        <video
+                            src="videos/feature-5.mp4"
+                            loop
+                            muted
+                            autoPlay
+                            className='size-full object-cover object-center'
+                        />
+                    </div>
+                </div>
+
             </div>
         </section>
     )
