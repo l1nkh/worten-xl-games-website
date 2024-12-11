ed1f43065986dc8a3f827cf6a9066bac170649d5
l1nkh <diogo.m.henriques98@gmail.com>
2024-12-11 22:59:16 +0000

    Finish contact section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({title, id, rightIcon, leftIcon, containerClass}) {
   return (
-    <button id={id} className={`group relative flex z-10 w-fit cursor-pointer 
+    <button id={id} className={`group relative flex z-10 w-fit cursor-pointer
     overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
         {leftIcon}
 
