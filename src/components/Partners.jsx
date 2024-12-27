9c22c3eb572f8d365775319f5941e53781e86f58
l1nkh <diogo.m.henriques98@gmail.com>
2024-12-27 01:37:03 +0000

    Update partners section of the website to highlight single partner with during scroll
    Still not finished

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const partnersList = [
     "worten",
@@ -22,14 +27,38 @@ const partnersList = [
     "oitbits.org",
     "waso",
     "Salao de jogos",
-]
+];
 
 const Partners = () => {
+    const itemsRef = useRef([]);
+
+    useGSAP(() => {
+        itemsRef.current.forEach((item, index) => {
+            if (item) {
+                ScrollTrigger.create({
+                    trigger: item,
+                    start: "top center",
+                    end: "bottom center",
+                    toggleActions: "play reverse play reverse",
+                    onEnter: () => item.classList.add('text-red-600'),
+                    onLeave: () => item.classList.remove('text-red-600'),
+                    onEnterBack: () => item.classList.add('text-red-600'),
+                    onLeaveBack: () => {
+                        if (index === 0) {
+                            item.classList.add('text-red-600');
+                        }else(
+                            item.classList.remove('text-red-600')
+                        )
+                    },
+                });
+            }
+        });
+    }, { revertOnUpdate: true });
+
     return (
         <section id='partners' className='min-h-screen w-screen py-72 bg-white text-black'>
             <div className="grid grid-cols-2 gap-7">
-                <div className="flex justify-self-end items-center w-1/2">
-
+                <div className="flex justify-self-end items-center w-1/2 col-span-1">
                     <p className="text-justify">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                         Exercitationem officiis quod veniam repudiandae ipsum
@@ -38,10 +67,17 @@ const Partners = () => {
                         porro odio inventore numquam.
                     </p>
                 </div>
-                <div className='flex flex-start'>
+                <div className='flex flex-start flex-col col-span-1'>
+                    <h1 className='bento-title special-font text-black'>
+                        Parceiros
+                    </h1>
                     <ul className='relative flex flex-col items-start'>
                         {partnersList.map((item, index) => (
-                            <li key={index} className='text-center bento-title !leading-[0.9]'>
+                            <li
+                                key={index}
+                                ref={(el) => (itemsRef.current[index] = el)}
+                                className={`text-center bento-title !leading-[0.9] text-black ${index === 0 ? 'text-red-600' : ''}`}
+                            >
                                 {item}
                             </li>
                         ))}
@@ -49,7 +85,7 @@ const Partners = () => {
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Partners
+export default Partners;
