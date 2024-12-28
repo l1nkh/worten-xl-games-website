2ba0e4f01623d850e8501493772365c5d6caa1de
l1nkh <diogo.m.henriques98@gmail.com>
2024-12-28 00:37:09 +0000

    Update footer colors to fit with the brand

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,18 +23,18 @@ const links = [
 
 const Footer = () => {
     return (
-        <footer className='w-screen bg-violet-300 py-4 text-black'>
+        <footer className='w-screen bg-red-600 py-4 text-white'>
             <div className='container mx-auto flex flex-col items-center
         justify-between gap-4 px-4 md:flex-row'>
                 <p className='text-center text-sm md:text-left'>
-                    &copy; Nova 2024. All rights reserved
+                    &copy; Diogo Henriques 2024. All rights reserved
                 </p>
 
                 <div className='flex justify-center gap-4 md:justify-start'>
                     {links.map((link => (
                         <a key={link} href={link.href} target='_blank' rel='noopener
-                    noreferrer' className='text-black transition-colors
-                    duration-500 ease-in-out hover:text-white'>
+                    noreferrer' className='text-white transition-colors
+                    duration-500 ease-in-out hover:text-black'>
                             {link.icon}
                         </a>
                     )))}
